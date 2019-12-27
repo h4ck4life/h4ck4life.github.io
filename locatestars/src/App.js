93aaa8fe2366ea0376d25f39a7b20fff94e1a8fc
Alif <alifaziz@gmail.com>
2019-12-27 16:03:18 +0800

    Resize and change texture

--- locatestars/src/App.js
+++ locatestars/src/App.js
@@ -12,9 +12,9 @@ function App() {
     <div>
       <a-scene /* embedded */ vr-mode-ui='false' arjs='sourceType: webcam; debugUIEnabled: false' fog="type: linear; color: #AAA">
         <a-assets>
-          <img id="boxTexture" src="https://i.imgur.com/mYmmbrp.jpg" />
+          <img crossOrigin="anonymous" id="boxTexture" src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Logo_of_Petron.svg" />
         </a-assets>
-        <a-box onClick={alertMessage} gps-entity-place="latitude: 3.127935; longitude: 101.472952;" src="#boxTexture" rotation="0 45 45" /* position="0 1.25 -15" */ scale="15 15 15" color="#EF2D5E" animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 1000; loop: true"></a-box>
+        <a-box onClick={alertMessage} gps-entity-place="latitude: 3.125351; longitude: 101.472123;" src="#boxTexture" rotation="0 45 45" /* position="0 1.25 -15" */ scale="25 25 125" color="#EF2D5E" animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 1000; loop: true"></a-box>
         <a-camera gps-camera rotation-reader>
           {/* <a-cursor></a-cursor> */}
         </a-camera>
