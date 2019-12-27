1c2cb6ec37d7af02ed04a07c513c92adf51a5052
Alif <alifaziz@gmail.com>
2019-12-28 02:47:18 +0800

    Finally a working version I guess

--- locatestars/src/App.js
+++ locatestars/src/App.js
@@ -5,18 +5,29 @@ import './App.css';
 function App() {
 
   let alertMessage = function (e) {
-    alert('clicked on box!');
+    //alert('clicked on box!');
   }
 
   return (
     <div>
       <a-scene /* embedded */ vr-mode-ui='false' arjs='sourceType: webcam; debugUIEnabled: false' fog="type: linear; color: #AAA">
         <a-assets>
-          <img crossOrigin="anonymous" id="boxTexture" src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Logo_of_Petron.svg" />
+          <img crossOrigin="anonymous" id="boxTexture" src="https://image.flaticon.com/icons/png/512/2230/2230346.png" />
+          <img crossOrigin="anonymous" id="prayermatTexture" src="https://image.flaticon.com/icons/png/512/1197/1197998.png" />
         </a-assets>
-        <a-box onClick={alertMessage} gps-entity-place="latitude: 3.125351; longitude: 101.472123;" src="#boxTexture" rotation="0 45 45" /* position="0 1.25 -15" */ scale="25 25 125" color="#EF2D5E" animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 1000; loop: true"></a-box>
-        <a-camera gps-camera rotation-reader>
-          {/* <a-cursor></a-cursor> */}
+        <a-box onClick={alertMessage}
+          gps-entity-place="latitude: 21.422487; longitude: 39.826206;"
+          /* gps-entity-place="latitude: 3.125351; longitude: 101.472123;" */
+          src="#boxTexture"
+          /* rotation="0 5 5" */
+          /* position="0 1.25 -15" */
+          scale="6 6 18"
+          /* animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 1000; loop: true" */>
+        </a-box>
+        <a-camera gps-camera="minDistance: 0;" rotation-reader>
+          <a-cursor>
+            <a-image width="0.17" height="0.1" src="#prayermatTexture"></a-image>
+          </a-cursor>
         </a-camera>
       </a-scene>
     </div>
