6c131227034d0e344ce5a6796e2dc968880b0a2a
Alif <alifaziz@gmail.com>
2019-12-27 15:51:39 +0800

    Update build url path

---
+++ locatestars/build/precache-manifest.9189e24d3db69ee6fe03865b450f76a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f729953e2dbbeab55167fae0defb7a38",
+    "url": "/locatestars/build/index.html"
+  },
+  {
+    "revision": "def2138f3718a2ef93ce",
+    "url": "/locatestars/build/static/css/main.7cc78d79.chunk.css"
+  },
+  {
+    "revision": "606a70885d58dbb05f0e",
+    "url": "/locatestars/build/static/js/2.e506a0e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/locatestars/build/static/js/2.e506a0e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "def2138f3718a2ef93ce",
+    "url": "/locatestars/build/static/js/main.455317d2.chunk.js"
+  },
+  {
+    "revision": "189fe2d3b83ac4954a20",
+    "url": "/locatestars/build/static/js/runtime-main.0ed3c495.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/locatestars/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- locatestars/build/service-worker.js
+++ locatestars/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96597529ec39dd9a2e41d3da3c50db74.js"
+  "/locatestars/build/precache-manifest.9189e24d3db69ee6fe03865b450f76a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/locatestars/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
